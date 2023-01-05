0c5c25f7413cd08695b4561b988610dd32a5767b
En221177 <88370443+En221177@users.noreply.github.com>
2023-01-05 18:44:25 +0800

    加入專案檔案。

---
+++ En_15_Ajax_Homework/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace En_15_Ajax_Homework.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
