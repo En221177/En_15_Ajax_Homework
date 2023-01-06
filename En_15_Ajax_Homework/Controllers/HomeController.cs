d80384fa5da636484c30c27f54833c65835798b0
En221177 <88370443+En221177@users.noreply.github.com>
2023-01-06 18:38:10 +0800

    20230104

--- En_15_Ajax_Homework/Controllers/HomeController.cs
+++ En_15_Ajax_Homework/Controllers/HomeController.cs
@@ -1,22 +1,39 @@
 ﻿using En_15_Ajax_Homework.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Text;
 
 namespace En_15_Ajax_Homework.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly DemoContext _context;
+        public HomeController(ILogger<HomeController> logger, DemoContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult register()
+        {
+            return View();
+        }
+
+        public IActionResult check(string name)
+        {
+            Member a = _context.Members.FirstOrDefault(x => x.Name.Contains(name));
+            if (a != null)
+                return Content($"{name}已經存在", "text/plane", Encoding.UTF8);
+            else 
+                return Content($"OK", "text/plane", Encoding.UTF8);
+        }
+
 
         public IActionResult Privacy()
         {
